88eb734364c576bba9f8e4e33912137795f12850
kingservice <kingservice2014@gmail.com>
2014-04-16 21:27:36 +0800

    fix

--- client/controller/main.js
+++ client/controller/main.js
@@ -2,4 +2,14 @@ Template.header.events({
   "click #logout" : function () {
     Meteor.logout(logoutCallback);
   }
+});
+
+
+Template.layout.events({
+	"click #logo" : function () {
+		Router.go("/dashboard");
+	},
+	"click #events": function () {
+		Router.go("/events");
+	}
 })
